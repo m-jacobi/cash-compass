751e8efad5db02a3528d0b09e77a966eba347fa5
Matthias Jacobi <mjacobi@mailbox.org>
2023-03-05 14:44:20 +0100

    implemented the german translation for the mat paginator

---
+++ src/app/utils/paginator-intl.ts
@@ -0,0 +1,22 @@
+import { MatPaginatorIntl } from '@angular/material/paginator';
+
+const rangeLabel = (page: number, pageSize: number, length: number) => {
+    if(length === 0) {
+        return 'Seite 1 von 1'
+    }
+
+    const totalPages = Math.ceil(length / pageSize);
+    return `Seite ${page + 1} von ${totalPages}`;
+}
+export const getMatPaginatorIntl = () => {
+    const paginatorIntl = new MatPaginatorIntl();
+
+    paginatorIntl.itemsPerPageLabel = 'Einträge pro Seite:';
+    paginatorIntl.nextPageLabel = 'Vor';
+    paginatorIntl.previousPageLabel = 'Zurück';
+    paginatorIntl.firstPageLabel = 'Anfang';
+    paginatorIntl.lastPageLabel = 'Ende';
+    paginatorIntl.getRangeLabel = rangeLabel;
+
+    return paginatorIntl;
+}
