5208dc82f81c20c8ce821115b006e9c6aa9ee2b2
Matthias Jacobi <mjacobi@mailbox.org>
2023-12-08 17:27:04 +0100

    fixed the category list and category dialog styling

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -11,7 +11,6 @@ import { CategoryModel, EMPTY_CATEGORY } from '../../core/models/category.model'
 import { PaymentModel } from '../../core/models/payment.model';
 import { CategoryModalDialogComponent } from '../../dialog/category-modal-dialog/category-modal-dialog.component';
 import { NOTIFICATION_TYPE } from '../../enum/notification-type.enum';
-import { DefaultCategory } from '../../models/default-category.model';
 import { NotificationService } from '../../services/notification.service';
 
 @Component({
@@ -21,15 +20,12 @@ import { NotificationService } from '../../services/notification.service';
 })
 export class CategoryListComponent implements OnInit, OnDestroy, AfterViewInit {
 
-    public categoryDataSource: MatTableDataSource<CategoryModel>;
-    public tableColumns: string[];
-    public defaultCategories: DefaultCategory[];
-    private categories: CategoryModel[];
-    private categoryIdsPerPayment: string[];
-
     @ViewChild(MatPaginator) public paginator: MatPaginator;
     @ViewChild(MatSort) public sort: MatSort;
 
+    public categoryDataSource: MatTableDataSource<CategoryModel>;
+    public categoryListTableColumns: string[];
+    private categoryIdsPerPayment: string[];
     private readonly ngDestroy = new Subject<void>();
 
     constructor(
@@ -38,51 +34,16 @@ export class CategoryListComponent implements OnInit, OnDestroy, AfterViewInit {
         private notificationService: NotificationService,
         private dialog: MatDialog,
     ) {
-        this.tableColumns = ['name', 'action'];
+        this.categoryListTableColumns = ['name', 'action'];
         this.categoryDataSource = new MatTableDataSource<CategoryModel>([]);
         this.paginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);
         this.sort = new MatSort;
-        this.categories = [];
         this.categoryIdsPerPayment = [];
-        this.defaultCategories = [
-            {
-                name: "---",
-                default: "noFilter",
-            },
-            {
-                name: "Ja",
-                default: true,
-            }, {
-                name: "Nein",
-                default: false,
-            }
-        ];
     }
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         this.getCategories();
         this.getCategoriesPerPayment();
-    }
-
-    public getCategories(): void {
-        this.categoryService.getCategories().pipe(takeUntil(this.ngDestroy))
-            .subscribe((categories: CategoryModel[]) => {
-                this.categories = categories;
-                this.categoryDataSource.data = this.sortCategoriesAsc(categories);
-         })
-    }
-
-    private sortCategoriesAsc(categories: CategoryModel[]): CategoryModel[] {
-        return categories.sort((a: CategoryModel, b: CategoryModel)=> a.name > b.name ? 1:-1 );
-    }
-
-    private getCategoriesPerPayment(): void {
-        this.paymentService.getPayments().pipe(takeUntil(this.ngDestroy))
-            .subscribe((payments: PaymentModel[]) => {
-                payments.forEach((payment: PaymentModel) => {
-                    this.categoryIdsPerPayment.push(payment.categoryId);
-                });
-            });
     }
 
     public ngAfterViewInit(): void {
@@ -98,21 +59,19 @@ export class CategoryListComponent implements OnInit, OnDestroy, AfterViewInit {
         this.ngDestroy.unsubscribe();
     }
 
+    public getCategories(): void {
+        this.categoryService.getCategories().pipe(takeUntil(this.ngDestroy))
+            .subscribe((categories: CategoryModel[]) => {
+                this.categoryDataSource.data = this.sortCategoriesAsc(categories);
+         })
+    }
+
     public filterCategories(event: Event): void  {
         const filterValue = (event.target as HTMLInputElement).value;
         this.categoryDataSource.filter = filterValue.trim().toLowerCase();
 
         if (this.categoryDataSource.paginator) {
             this.categoryDataSource.paginator.firstPage();
-        }
-    }
-
-    public filterDefaultCategory(event: any): void {
-        if (event.value != "noFilter") {
-            const filteredDataByDefaultCategory = this.categories.filter((category: CategoryModel) => category.defaultCategory === event.value);
-            this.categoryDataSource.data = filteredDataByDefaultCategory;
-        } else {
-            this.categoryDataSource.data = this.categories;
         }
     }
 
@@ -144,6 +103,19 @@ export class CategoryListComponent implements OnInit, OnDestroy, AfterViewInit {
                 buttonText: 'OK'
             });
         }
+    }
+
+    private sortCategoriesAsc(categories: CategoryModel[]): CategoryModel[] {
+        return categories.sort((a: CategoryModel, b: CategoryModel)=> a.name > b.name ? 1:-1 );
+    }
+
+    private getCategoriesPerPayment(): void {
+        this.paymentService.getPayments().pipe(takeUntil(this.ngDestroy))
+            .subscribe((payments: PaymentModel[]) => {
+                payments.forEach((payment: PaymentModel) => {
+                    this.categoryIdsPerPayment.push(payment.categoryId);
+                });
+            });
     }
 
     private categoryIsUsedForPayments(categoryId: string, categoryIdsPerPayment: string[]): any {
