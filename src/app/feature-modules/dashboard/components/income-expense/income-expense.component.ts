cde20297936857e7318c0e07a0bfe0689ec808ee
Matthias Jacobi <mjacobi@mailbox.org>
2024-03-25 19:55:38 +0100

    wip

--- src/app/feature-modules/dashboard/components/income-expense/income-expense.component.ts
+++ src/app/feature-modules/dashboard/components/income-expense/income-expense.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject } from 'rxjs';
+import { PaymentFacade } from '../../../../core/facades/payment.facade';
 import { PaymentListVM } from '../../../payment/models/payment.vm';
 import { PaymentListPresenter } from '../../../payment/presenter/payment-list.presenter';
 
@@ -20,16 +21,34 @@ export class IncomeExpenseComponent implements OnInit {
 
 
 
-    constructor(private paymentListPresenter: PaymentListPresenter) {}
+    constructor(
+        private paymentFacade: PaymentFacade,
+        private paymentListPresenter: PaymentListPresenter
+    ) {}
 
     public ngOnInit() {
+        this.paymentFacade.loadPayments;
         this.loadPayments();
+
+        // const data = [
+        //     { id: 1, name: 'Red', parentId: null },
+        //     { id: 2, name: 'Apple', parentId: 1 },
+        //     { id: 3, name: 'Stop sign', parentId: 1 },
+        //     { id: 4, name: 'Cake', parentId: 2 },
+        //   ]
+
+        //   const source = of(data).pipe(
+        //     mergeAll(),
+        //     groupBy((item) => item.parentId),
+        //     mergeMap(group => group.pipe(toArray()))
+        //   );
+
+        //   source.subscribe(x => console.log(x));
     }
 
     private loadPayments(): void {
-        this.paymentListPresenter.paymentsVm$.pipe(takeUntil(this.ngDestroy)).subscribe((payments: PaymentListVM[]) => {
-            this.payments = payments;
-            console.log('payments', payments);
-            });
+        // this.paymentListPresenter.paymentsVm$.subscribe((p: PaymentListVM[]) => {
+        //     console.log('p', p);
+        // });
     }
 }
